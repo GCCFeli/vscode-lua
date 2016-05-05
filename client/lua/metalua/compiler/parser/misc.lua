3a0c1d660c7fececd8eff570527be675f1b5feb2
hanqing.cui <hanqing.cui@capstonegaming.com>
2016-05-05 16:23:15 +0800

    remove meta stuff

--- client/lua/metalua/compiler/parser/misc.lua
+++ client/lua/metalua/compiler/parser/misc.lua
@@ -44,7 +44,7 @@ local gg = require 'metalua.grammar.generator'
 return function(M)
     local _M = gg.future(M)
 
---[[ metaprog-free versions:
+    metaprog-free versions:
     function M.id(lx)
         if lx:peek().tag~='Id' then gg.parse_error(lx, "Identifier expected")
         else return lx:next() end
@@ -71,75 +71,11 @@ return function(M)
         if id.tag == "Id" then id.tag = "String"; return id
         else error ("Identifier expected: "..table.tostring(id, 'nohash')) end
     end
---]]
-
-    --------------------------------------------------------------------------------
-    -- Try to read an identifier (possibly as a splice), or return [false] if no
-    -- id is found.
-    --------------------------------------------------------------------------------
-    function M.opt_id (lx)
-        local a = lx:peek();
-        if lx:is_keyword (a, "-{") then
-            local v = M.meta.splice(lx)
-            if v.tag ~= "Id" and v.tag ~= "Splice" then
-                gg.parse_error(lx, "Bad id splice")
-            end
-            return v
-        elseif a.tag == "Id" then return lx:next()
-        else return false end
-    end
-
-    --------------------------------------------------------------------------------
-    -- Mandatory reading of an id: causes an error if it can't read one.
-    --------------------------------------------------------------------------------
-    function M.id (lx)
-        return M.opt_id (lx) or gg.parse_error(lx,"Identifier expected")
-    end
 
     --------------------------------------------------------------------------------
     -- Common helper function
     --------------------------------------------------------------------------------
     M.id_list = gg.list { primary = _M.id, separators = "," }
-
-    --------------------------------------------------------------------------------
-    -- Converts an identifier into a string. Hopefully one day it'll handle
-    -- splices gracefully, but that proves quite tricky.
-    --------------------------------------------------------------------------------
-    function M.id2string (id)
-        --print("id2string:", disp.ast(id))
-        if id.tag == "Id" then id.tag = "String"; return id
-        elseif id.tag == "Splice" then
-            error ("id2string on splice not implemented")
-            -- Evaluating id[1] will produce `Id{ xxx },
-            -- and we want it to produce `String{ xxx }.
-            -- The following is the plain notation of:
-            -- +{ `String{ `Index{ `Splice{ -{id[1]} }, `Number 1 } } }
-            return { tag="String",  { tag="Index", { tag="Splice", id[1] },
-                                      { tag="Number", 1 } } }
-        else error ("Identifier expected: "..pp.tostring (id, {metalua_tag=1, hide_hash=1})) end
-    end
-
-    --------------------------------------------------------------------------------
-    -- Read a string, possibly spliced, or return an error if it can't
-    --------------------------------------------------------------------------------
-    function M.string (lx)
-        local a = lx:peek()
-        if lx:is_keyword (a, "-{") then
-            local v = M.meta.splice(lx)
-            if v.tag ~= "String" and v.tag ~= "Splice" then
-                gg.parse_error(lx,"Bad string splice")
-            end
-            return v
-        elseif a.tag == "String" then return lx:next()
-        else error "String expected" end
-    end
-
-    --------------------------------------------------------------------------------
-    -- Try to read a string, or return false if it can't. No splice allowed.
-    --------------------------------------------------------------------------------
-    function M.opt_string (lx)
-        return lx:peek().tag == "String" and lx:next()
-    end
 
     --------------------------------------------------------------------------------
     -- Chunk reader: block + Eof
